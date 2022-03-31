1f0f4abcbc7fdeb64f80d6d2d2ea01c7cae8ff1b
Alessandro Rossi <S4641902@studenti.unige.it>
2022-03-31 23:52:57 +0200

    Added player movement and lasers

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 #![allow(unused)]
 
 
+use std::borrow::BorrowMut;
 use std::path::Path;
+use std::process::exit;
+use std::ptr::addr_of;
+use bevy::app::AppExit;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy::render::texture::ImageType;
+use bevy::window::CloseWindow;
 use crate::StartupStage::Startup;
 
 const SPRITE_DIR: &str = "assets";
-pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
+const LASER_SPRITE: &str = "laser_a_01.png";
+
+const TIME_STEP: f32 = 1. / 60.;
 
 pub struct SpriteInfos {
-    player: (Handle<Image>, Vec2)
+    player: (Handle<Image>, Vec2),
+    laser: (Handle<Image>, Vec2)
 }
 
 struct WinSize {
@@ -20,9 +29,42 @@ struct WinSize {
     h: f32,
 }
 
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_startup_stage(
+                "game_setup_actors",
+                SystemStage::single(player_spawn.system()),
+            )
+            .add_system(player_movement.system())
+            .add_system(player_fire.system())
+            .add_system(laser_movement.system());
+
+    }
+}
+
+#[derive(Component)]
+struct Player;
+#[derive(Component)]
+struct Laser;
+#[derive(Component)]
+struct Speed(f32);
+
+impl Default for Speed {
+    fn default() -> Self {
+        Self(500.)
+    }
+}
+
 fn main() {
     App::new()
+
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .insert_resource(WindowDescriptor {
             title: "Rust Invaders!".to_string(),
             width: 598.0,
@@ -30,8 +72,9 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup)
-        .add_system(player_spawn.system())
+        .add_system(close_game)
         .run();
 }
 
@@ -44,7 +87,6 @@ fn setup(
     mut windows: ResMut<Windows>
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
     let mut window = windows.get_primary_mut().unwrap();
 
     commands.insert_resource(WinSize{
@@ -55,7 +97,8 @@ fn setup(
     window.set_position(IVec2::new(1300, 80));
 
     commands.insert_resource(SpriteInfos {
-        player: load_image(&mut images, PLAYER_SPRITE)
+        player: load_image(&mut images, PLAYER_SPRITE),
+        laser: load_image(&mut images, LASER_SPRITE)
     });
 
 }
@@ -83,5 +126,70 @@ fn player_spawn(mut commands: Commands, asset_server: Res<AssetServer>, win_size
             ..Default::default()
         },
         ..Default::default()
-    });
+    })
+        .insert(Player)
+        .insert(Speed::default());
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Speed, &mut Transform), With<Player>>,
+    win_size: Res<WinSize>
+) {
+    let left_boundary = -win_size.w / 2.;
+    let right_boundary = win_size.w / 2.;
+    if let Ok((speed, mut transform)) = query.get_single_mut() {
+        let direction = if keyboard_input.pressed(KeyCode::A) { -1.}
+        else if keyboard_input.pressed(KeyCode::D)  { 1.}
+        else { 0.};
+        transform.translation.x += direction * speed.0 * TIME_STEP;
+    }
+}
+
+fn close_game(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut exit: EventWriter<AppExit>
+) {
+    if keyboard_input.pressed(KeyCode::Escape) {
+        exit.send(AppExit);
+    }
+}
+
+fn player_fire(
+    mut commands: Commands,
+    kb: Res<Input<KeyCode>>,
+    asset_server: Res<AssetServer>,
+    mut query: Query<&Transform , With<Player>>
+){
+    if let Ok(player_tf) = query.get_single_mut() {
+        if kb.pressed(KeyCode::Space) {
+            let coords = (player_tf.translation.x, player_tf.translation.y);
+            let image = asset_server.load(LASER_SPRITE);
+            commands.spawn_bundle(SpriteBundle {
+                texture: image,
+                transform: Transform {
+                    translation: Vec3::new(coords.0, coords.1, 0.),
+                    scale: Vec3::new(0.5, 0.5, 0.1),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+                .insert(Laser)
+                .insert(Speed::default());
+        }
+    }
+}
+
+fn laser_movement(
+    mut commands: Commands,
+    win_size: Res<WinSize>,
+    mut query: Query<(Entity, &Speed, &mut Transform), With<Laser>>
+) {
+    for (laser_entity, speed, mut laser_tf) in query.iter_mut() {
+        let translation = &mut laser_tf.translation;
+        translation.y += speed.0 * TIME_STEP;
+        // if translation.y > win_size.h {
+        //     commands.entity(laser_entity).despawn();
+        // }
+    }
 }
